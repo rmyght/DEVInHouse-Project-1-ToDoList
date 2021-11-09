adbc23e64fc05278878bf17d0d7184137bd2b649
rmyght <ramon.stelles@gmail.com>
2021-11-08 22:36:01 -0300

    Adicionado o botão de adicionar tarefa com o checkbox

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Adiciona uma tarefa na lista:
+function addTodo() {
+    let tr = document.createElement('tr');
+    document.getElementById('todo-body-list').appendChild(tr);
+    
+    // Adiciona o Checkbox na Lista.
+    let tdCheck = document.createElement('td');
+    tr.appendChild(tdCheck);
+    let checkBox = document.createElement('INPUT');
+    checkBox.setAttribute('type', 'checkbox');
+    tdCheck.appendChild(checkBox);
+
+    let tdTodo = document.createElement('td');
+    tdTodo.textContent = 'Caminhar';
+    tr.appendChild(tdTodo);
+
+
+    let tdDel = document.createElement('td');
+    tdDel.textContent = 'Caminhar';
+    tr.appendChild(tdDel);
+}
