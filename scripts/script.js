917cd5018105f6b321388a08957da1b51bad70de
rmyght <ramon.stelles@gmail.com>
2021-11-09 14:45:59 -0300

    Adicionada função do botão deletar; Melhoria que limpa o input text quando adicionado na lista.

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,14 @@ function isEmpty(str) {
 
 // Adiciona uma tarefa na lista:
 function addTodo() {
-    let task = document.getElementById("task").value
-    if (isEmpty(task)){
+    let newTask = document.getElementById("new-task").value
+    if (isEmpty(newTask)){
         console.log("Test")
     } else {
+        let newTaskID = newTask.toLowerCase().replace(/\s/g, '-');
         let tr = document.createElement("tr");
         document.getElementById("todo-body-list").appendChild(tr);
+        tr.id = newTaskID;
         
         // Adiciona o Checkbox na Lista.
         let tdCheck = document.createElement("td");
@@ -20,13 +22,16 @@ function addTodo() {
         tdCheck.appendChild(checkBox);
 
         let tdTodo = document.createElement("td");
-        tdTodo.textContent = task;
+        tdTodo.textContent = newTask;
         tr.appendChild(tdTodo);
 
         let tdDel = document.createElement("td");
         tr.appendChild(tdDel);
         let delBtn = document.createElement("BUTTON");
+        delBtn.onclick = function() {document.getElementById(this.parentNode.parentNode.id).remove();};
         delBtn.textContent = "X";
         tdDel.appendChild(delBtn);
+
+        document.getElementById("new-task").value = ''
     }
 }
