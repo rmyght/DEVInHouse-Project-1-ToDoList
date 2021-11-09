c0871423687395653d9ced9d565273f67cd82549
rmyght <ramon.stelles@gmail.com>
2021-11-08 22:59:17 -0300

    Adicionado botão de deletar (ainda sem função)

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,23 @@
 // Adiciona uma tarefa na lista:
 function addTodo() {
-    let tr = document.createElement('tr');
-    document.getElementById('todo-body-list').appendChild(tr);
+    let tr = document.createElement("tr");
+    document.getElementById("todo-body-list").appendChild(tr);
     
     // Adiciona o Checkbox na Lista.
-    let tdCheck = document.createElement('td');
+    let tdCheck = document.createElement("td");
     tr.appendChild(tdCheck);
-    let checkBox = document.createElement('INPUT');
-    checkBox.setAttribute('type', 'checkbox');
+    let checkBox = document.createElement("INPUT");
+    checkBox.setAttribute("type", "checkbox");
     tdCheck.appendChild(checkBox);
 
-    let tdTodo = document.createElement('td');
-    tdTodo.textContent = 'Caminhar';
+    let tdTodo = document.createElement("td");
+    tdTodo.textContent = "Caminhar";
     tr.appendChild(tdTodo);
 
 
-    let tdDel = document.createElement('td');
-    tdDel.textContent = 'Caminhar';
+    let tdDel = document.createElement("td");
     tr.appendChild(tdDel);
+    let delBtn = document.createElement("BUTTON");
+    delBtn.textContent = "X";
+    tdDel.appendChild(delBtn);
 }
