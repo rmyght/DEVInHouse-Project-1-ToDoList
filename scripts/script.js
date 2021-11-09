a80378d47ef883396286d4fec6222f81f375e2d4
rmyght <ramon.stelles@gmail.com>
2021-11-09 12:17:13 -0300

    Input Text agora é capturado e jogado como tarefa; Adicionado controle para que a tarefa não seja em branco.

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,32 @@
+// Verifica se a String está vazia.
+function isEmpty(str) {
+    return (!str || /^\s*$/.test(str));
+}
+
 // Adiciona uma tarefa na lista:
 function addTodo() {
-    let tr = document.createElement("tr");
-    document.getElementById("todo-body-list").appendChild(tr);
-    
-    // Adiciona o Checkbox na Lista.
-    let tdCheck = document.createElement("td");
-    tr.appendChild(tdCheck);
-    let checkBox = document.createElement("INPUT");
-    checkBox.setAttribute("type", "checkbox");
-    tdCheck.appendChild(checkBox);
+    let task = document.getElementById("task").value
+    if (isEmpty(task)){
+        console.log("Test")
+    } else {
+        let tr = document.createElement("tr");
+        document.getElementById("todo-body-list").appendChild(tr);
+        
+        // Adiciona o Checkbox na Lista.
+        let tdCheck = document.createElement("td");
+        tr.appendChild(tdCheck);
+        let checkBox = document.createElement("INPUT");
+        checkBox.setAttribute("type", "checkbox");
+        tdCheck.appendChild(checkBox);
 
-    let tdTodo = document.createElement("td");
-    tdTodo.textContent = "Caminhar";
-    tr.appendChild(tdTodo);
+        let tdTodo = document.createElement("td");
+        tdTodo.textContent = task;
+        tr.appendChild(tdTodo);
 
-
-    let tdDel = document.createElement("td");
-    tr.appendChild(tdDel);
-    let delBtn = document.createElement("BUTTON");
-    delBtn.textContent = "X";
-    tdDel.appendChild(delBtn);
+        let tdDel = document.createElement("td");
+        tr.appendChild(tdDel);
+        let delBtn = document.createElement("BUTTON");
+        delBtn.textContent = "X";
+        tdDel.appendChild(delBtn);
+    }
 }
