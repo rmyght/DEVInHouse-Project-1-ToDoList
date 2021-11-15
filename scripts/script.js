e52a61e1d1307d2faef7dd83fc9dc2412ab6c5bc
rmyght <ramon.stelles@gmail.com>
2021-11-15 13:30:25 -0300

    Ajustado o botão de Done (checkbox); Ao clicar em Done (checkbox) a Task irá ficar line-through; Finalizado o Local Storage; Melhorias do código em geral.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-//// Métodos e ou Funções Privadas ////
+//// Métodos e/ou Funções Privadas ////
 // Verifica se a String está vazia.
 function _isEmpty(str) {
-    return (!str || /^\s*$/.test(str))
+    return (!str || /^\s*$/.test(str));
 }
 
 // Verifica se já existe uma key no Local Storage
 function _existsLS(key) {
-    return localStorage.getItem(key)
+    return localStorage.getItem(key);
 }
 
 // Seta um item em Local Storage
@@ -18,63 +18,44 @@ function _setItemLS(key, item){
 function _addLS(newTask, newTaskID, localTasks) {
     if (localTasks) {
         let newLocalTask = JSON.parse(localTasks);
-        newLocalTask[newTaskID] = {checkBox:`check-${newTaskID}`, checked:0, taskID:`task-${newTaskID}`, task:newTask, delButton:`del-${newTaskID}`}
-        _setItemLS("tasks", JSON.stringify(newLocalTask))
+        newLocalTask[newTaskID] = {checkBox:`check-${newTaskID}`, checked:0, taskID:`task-${newTaskID}`, task:newTask, delButton:`del-${newTaskID}`};
+        _setItemLS("tasks", JSON.stringify(newLocalTask));
     } else {
         let objectTask = {[`${newTaskID}`]:{checkBox:`check-${newTaskID}`, checked:0, taskID:`task-${newTaskID}`, task:newTask, delButton:`del-${newTaskID}`}};
-        _setItemLS("tasks", JSON.stringify(objectTask))
+        _setItemLS("tasks", JSON.stringify(objectTask));
     }
 }
 
-// Antiga criação de tabela de exibição no HTML
-function _createTaskTable(newTask, newTaskID) {
-    let tr = document.createElement("tr");
-    document.getElementById("task-body-list").appendChild(tr);
-    tr.id = newTaskID;
-    // Adiciona o Checkbox na Lista.
-    let tdCheck = document.createElement("td");
-    tdCheck.id = `check-${newTaskID}`;
-    tr.appendChild(tdCheck);
-    let checkBox = document.createElement("INPUT");
-    checkBox.setAttribute("type", "checkbox");
-    checkBox.onclick = function() {console.log("Teste")}; // Ajustar Checkbox
-    tdCheck.appendChild(checkBox);
+// // Salva o checked como 1 ou 0 no Local Storage
+// function _checkUncheckBox(id, checked) {
+//     let objectLocalTask = JSON.parse(actualLocalTasks);
+//     for (let key in objectLocalTask) {
+//         if (key === id){
+//             childObj = objectLocalTask[key];
+//             for (let childKey in childObj) {
+//                 if (childKey === "checked") {
+//                     childObj[childKey] = checked;
+//                     _setItemLS("tasks", JSON.stringify(objectLocalTask));
+//                 }
+//             }
+//         }
+//     }
+// }
 
-    // Adiciona o nome da Tarefa na Lista
-    let tdTask = document.createElement("td");
-    tdTask.textContent = newTask;
-    tdTask.id = `task-${newTaskID}`;
-    tr.appendChild(tdTask);
-
-    // Adiciona o botão de deletar na Lista.
-    let tdDel = document.createElement("td");
-    tdDel.id = `del-${newTaskID}`;
-    tr.appendChild(tdDel);
-    let delBtn = document.createElement("BUTTON");
-    delBtn.onclick = function() {document.getElementById(this.parentNode.parentNode.id).remove();};
-    delBtn.textContent = "X";
-    tdDel.appendChild(delBtn);
-
-    document.getElementById("new-task").value = '';
-}
-
+// Salva o checked como 1 ou 0 no Local Storage
 function _checkUncheckBox(id, checked) {
     let objectLocalTask = JSON.parse(actualLocalTasks);
     for (let key in objectLocalTask) {
         if (key === id){
             childObj = objectLocalTask[key];
-            for (let childKey in childObj) {
-                if (childKey === "checked"){
-                    childObj[childKey] = checked
-                    _setItemLS("tasks", JSON.stringify(objectLocalTask))
-                }
-            }
+            childObj['checked'] = checked;
+            _setItemLS("tasks", JSON.stringify(objectLocalTask));
         }
     }
 }
 
 // Cria a tabela de exibição no HTML
-function _createLocalStorageTaskTable(actualLocalStorage, newTask, newTaskID) {
+function _createLocalStorageTaskTable(actualLocalStorage) {
     console.log(actualLocalStorage)
     if (actualLocalStorage) {
         let objectLocalTask = JSON.parse(actualLocalStorage);
@@ -92,26 +73,27 @@ function _createLocalStorageTaskTable(actualLocalStorage, newTask, newTaskID) {
                     tr.appendChild(tdCheck);
                     let checkBox = document.createElement("INPUT");
                     checkBox.setAttribute("type", "checkbox");
-                     // Não está funcionando.
-                    // checkBox.checked = childObj['checked'];
+                    checkBox.checked = childObj['checked'];
                     checkBox.onclick = function() {
                                             let checkBoxElement = document.getElementById(this.parentNode.id);
-                                            let checkBoxId = checkBoxElement.parentNode.id;
-                                            let siblingElement = checkBoxElement.nextElementSibling;
-                                            if (siblingElement.classList.contains('done')){
-                                                siblingElement.classList.remove('done')
-                                                _checkUncheckBox(checkBoxId, 0);
+                                            let checkBoxID = checkBoxElement.parentNode.id;
+                                            let siblingElementID = document.getElementById(`${checkBoxElement.nextElementSibling.id}`);
+                                            if (siblingElementID.classList.contains('done')){
+                                                siblingElementID.classList.remove('done');
+                                                _checkUncheckBox(checkBoxID, 0);
                                             } else {
-                                                siblingElement.classList.add('done')
-                                                _checkUncheckBox(checkBoxId, 1);
+                                                siblingElementID.classList.add('done');
+                                                _checkUncheckBox(checkBoxID, 1);
                                             }
-                                        }; // Ajustar Checkbox
+                                            location.reload(true);
+                                        };
                     tdCheck.appendChild(checkBox);
                 } else if (childKey == "task") {
                     // Adiciona o nome da Tarefa na Lista
                     let tdTask = document.createElement("td");
                     tdTask.textContent = childObj[childKey];
-                    // tdTask.id = `task-${newTaskID}`;
+                    tdTask.id = childObj['taskID'];
+                    if (childObj['checked'] === 1) tdTask.className = "done";
                     tr.appendChild(tdTask);
                 } else if (childKey == "delButton") {
                     // Adiciona o botão de deletar na Lista.
@@ -124,7 +106,7 @@ function _createLocalStorageTaskTable(actualLocalStorage, newTask, newTaskID) {
                                             delID = document.getElementById(this.parentNode.parentNode.id).id;
                                             delete objectLocalTask[delID];
                                             _setItemLS("tasks", JSON.stringify(objectLocalTask));
-                                            location.reload();
+                                            location.reload(true);
                                         };
                     delBtn.textContent = "X";
                     tdDel.appendChild(delBtn);
@@ -137,11 +119,11 @@ function _createLocalStorageTaskTable(actualLocalStorage, newTask, newTaskID) {
 
 // Retorna o número do ID da Task
 function _returnTaskNumber(number){
-    if (number) return parseInt(number) + 1
-    return 1
+    if (number) return parseInt(number) + 1;
+    return 1;
 }
 
-//// Métodos e ou Funções Publicas ////
+//// Métodos e/ou Funções Publicas ////
 // Limpa o Local Storage
 function clearStorage() {
     localStorage.clear();
@@ -168,4 +150,4 @@ function addTask() {
 let newTaskID = _returnTaskNumber(null);
 
 let actualLocalTasks = _existsLS("tasks");
-_createLocalStorageTaskTable(actualLocalTasks)
+_createLocalStorageTaskTable(actualLocalTasks);
